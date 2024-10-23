dbe9bc261d193fcadf1897c81497d6d771992f5b
RajMogare <rajmogare.tg@gmail.com>
2024-10-23 08:34:20 +0530

    small change

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,47 +1,46 @@
- 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
- //@ts-nocheck
- import React from "react";
- import {
-   FUNDING,
-   PayPalButtons,
-   PayPalScriptProvider,
- } from "@paypal/react-paypal-js";
- 
- interface PayPalButtonProps {
-   amount: string;
-   onSuccess: (details) => void;
- }
- 
- const PayPalButton = ({ amount, onSuccess }: PayPalButtonProps) => {
-   return (
-     <PayPalScriptProvider
-       options={{
-         clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
-         currency: "USD",
-       }}
-     >
-       <PayPalButtons
-         fundingSource={FUNDING.PAYPAL}
-         createOrder={(data, actions) => {
-           return actions.order.create({
-             purchase_units: [
-               {
-                 amount: {
-                   value: amount,
-                 },
-               },
-             ],
-           });
-         }}
-         onApprove={(data, actions) => {
-           return actions.order.capture().then((details) => {
-             onSuccess(details);
-           });
-         }}
-       />
-     </PayPalScriptProvider>
-   );
- };
- 
- export default PayPalButton; 
+//@ts-nocheck
+
+import React from "react";
+import {
+  FUNDING,
+  PayPalButtons,
+  PayPalScriptProvider,
+} from "@paypal/react-paypal-js";
+
+interface PayPalButtonProps {
+  amount: string;
+  onSuccess: (details: Record<string, any>) => void;  // or use a more specific type if available
+}
+
+const PayPalButton = ({ amount, onSuccess }: PayPalButtonProps) => {
+  return (
+    <PayPalScriptProvider
+      options={{
+        clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
+        currency: "USD",
+      }}
+    >
+      <PayPalButtons
+        fundingSource={FUNDING.PAYPAL}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: amount,
+                },
+              },
+            ],
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions.order.capture().then((details) => {
+            onSuccess(details);  // Pass the captured details to the success handler
+          });
+        }}
+      />
+    </PayPalScriptProvider>
+  );
+};
+
+export default PayPalButton;
